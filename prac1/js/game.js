1024a93e1ce3ab04147679561848505c9f0adff5
skuzow <alex@skuzow.net>
2024-03-03 16:48:18 +0100

    feat: new icons & better music naming

--- prac1/js/game.js
+++ prac1/js/game.js
@@ -73,11 +73,11 @@ var game = {
   startBackgroundMusic: function () {
     var toggleImage = $('#togglemusic')[0];
     game.backgroundMusic.play();
-    toggleImage.src = 'images/icons/sound.png';
+    toggleImage.src = 'images/icons/music.png';
   },
   stopBackgroundMusic: function () {
     var toggleImage = $('#togglemusic')[0];
-    toggleImage.src = 'images/icons/nosound.png';
+    toggleImage.src = 'images/icons/nomusic.png';
     game.backgroundMusic.pause();
     game.backgroundMusic.currentTime = 0; // Ir al comienzo de la canción
   },
@@ -85,10 +85,10 @@ var game = {
     var toggleImage = $('#togglemusic')[0];
     if (game.backgroundMusic.paused) {
       game.backgroundMusic.play();
-      toggleImage.src = 'images/icons/sound.png';
+      toggleImage.src = 'images/icons/music.png';
     } else {
       game.backgroundMusic.pause();
-      $('#togglemusic')[0].src = 'images/icons/nosound.png';
+      $('#togglemusic')[0].src = 'images/icons/nomusic.png';
     }
   },
   showLevelScreen: function () {
