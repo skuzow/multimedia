08084d2a6bd3cba04fdf9f79ce15448d0938cc49
skuzow <alex@skuzow.net>
2024-05-16 17:18:05 +0200

    feat(prac3): header scroll to sections

---
+++ prac3/src/constants/navItems.ts
@@ -0,0 +1,23 @@
+interface NavItem {
+  url: string;
+  label: string;
+  text: string;
+}
+
+export const NAVITEMS: NavItem[] = [
+  {
+    url: '/#what-to-expect',
+    label: 'what-to-expect',
+    text: 'What to expect'
+  },
+  {
+    url: '/#rides-and-shows',
+    label: 'rides-and-shows',
+    text: 'Rides & Shows'
+  },
+  {
+    url: '/#plan-your-visit',
+    label: 'plan-your-visit',
+    text: 'Plan your visit'
+  }
+];
--- prac3/src/constants/travelerPhotos.ts
+++ prac3/src/constants/travelerPhotos.ts
@@ -1,31 +1,31 @@
 export interface Photo {
-    id: string;
-    title: string;
-    description: string;
-    imageUrl: string;
-    summary: string;
+  id: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+  summary: string;
+}
+
+export const PHOTOS: Photo[] = [
+  {
+    id: 'activity-1',
+    title: 'Activity 1',
+    description: 'Description of Activity 1',
+    imageUrl: '/traveler-photos/pic-1.jpg',
+    summary: 'Summary of Activity 1'
+  },
+  {
+    id: 'activity-2',
+    title: 'Activity 2',
+    description: 'Description of Activity 2',
+    imageUrl: '/traveler-photos/pic-2.webp',
+    summary: 'Summary of Activity 2'
+  },
+  {
+    id: 'activity-3',
+    title: 'Activity 3',
+    description: 'Description of Activity 3',
+    imageUrl: '/traveler-photos/pic-3.jpg',
+    summary: 'Summary of Activity 3'
   }
-  export const photos: Photo[] = [
-    {
-      id: 'activity-1',
-      title: 'Activity 1',
-      description: 'Description of Activity 1',
-      imageUrl: 'traveler-photos/pic-1.jpg',
-      summary: 'Summary of Activity 1',
-    },
-    {
-      id: 'activity-2',
-      title: 'Activity 2',
-      description: 'Description of Activity 2',
-      imageUrl: 'traveler-photos/pic-2.webp',
-      summary: 'Summary of Activity 2',
-    },
-    {
-      id: 'activity-3',
-      title: 'Activity 3',
-      description: 'Description of Activity 3',
-      imageUrl: 'traveler-photos/pic-3.jpg',
-      summary: 'Summary of Activity 3',
-    }
-  ];
-  
+];
