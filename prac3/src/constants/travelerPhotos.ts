cee788567e54db7c16e1ee42e641d658358cba3a
gutche <gledrianregalagutierrez@gmail.com>
2024-05-21 00:04:30 +0200

    feat: add upload photo feature

--- prac3/src/constants/travelerPhotos.ts
+++ prac3/src/constants/travelerPhotos.ts
@@ -27,5 +27,40 @@ export const PHOTOS: Photo[] = [
     description: 'Description of Activity 3',
     imageUrl: '/traveler-photos/pic-3.webp',
     summary: 'Summary of Activity 3'
+  },
+  {
+    id: 'activity-1',
+    title: 'Activity 1',
+    description: 'Description of Activity 1',
+    imageUrl: '/traveler-photos/pic-1.webp',
+    summary: 'Summary of Activity 1'
+  },
+  {
+    id: 'activity-2',
+    title: 'Activity 2',
+    description: 'Description of Activity 2',
+    imageUrl: '/traveler-photos/pic-2.webp',
+    summary: 'Summary of Activity 2'
+  },
+  {
+    id: 'activity-3',
+    title: 'Activity 3',
+    description: 'Description of Activity 3',
+    imageUrl: '/traveler-photos/pic-3.webp',
+    summary: 'Summary of Activity 3'
+  },
+  {
+    id: 'activity-2',
+    title: 'Activity 2',
+    description: 'Description of Activity 2',
+    imageUrl: '/traveler-photos/pic-2.webp',
+    summary: 'Summary of Activity 2'
+  },
+  {
+    id: 'activity-3',
+    title: 'Activity 3',
+    description: 'Description of Activity 3',
+    imageUrl: '/traveler-photos/pic-3.webp',
+    summary: 'Summary of Activity 3'
   }
 ];
