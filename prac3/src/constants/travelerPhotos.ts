84ecbee6230be2f06107f103908c54896d86360d
skuzow <alex@skuzow.net>
2024-05-20 20:28:45 +0200

    feat(prac3): optimize traveler photos images

--- prac3/src/constants/travelerPhotos.ts
+++ prac3/src/constants/travelerPhotos.ts
@@ -11,7 +11,7 @@ export const PHOTOS: Photo[] = [
     id: 'activity-1',
     title: 'Activity 1',
     description: 'Description of Activity 1',
-    imageUrl: '/traveler-photos/pic-1.jpg',
+    imageUrl: '/traveler-photos/pic-1.webp',
     summary: 'Summary of Activity 1'
   },
   {
@@ -25,7 +25,7 @@ export const PHOTOS: Photo[] = [
     id: 'activity-3',
     title: 'Activity 3',
     description: 'Description of Activity 3',
-    imageUrl: '/traveler-photos/pic-3.jpg',
+    imageUrl: '/traveler-photos/pic-3.webp',
     summary: 'Summary of Activity 3'
   }
 ];
