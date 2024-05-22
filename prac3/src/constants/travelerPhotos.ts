10a5f9ee40bdaac36baf8ff1ca3ecf6c02081967
skuzow <alex@skuzow.net>
2024-05-22 05:54:19 +0200

    feat(prac3): upload traveler photo modal & alert

--- prac3/src/constants/travelerPhotos.ts
+++ prac3/src/constants/travelerPhotos.ts
@@ -1,66 +1,39 @@
 export interface Photo {
-  id: string;
   title: string;
-  description: string;
   imageUrl: string;
-  summary: string;
 }
 
 export const PHOTOS: Photo[] = [
   {
-    id: 'activity-1',
-    title: 'Activity 1',
-    description: 'Description of Activity 1',
-    imageUrl: '/traveler-photos/pic-1.webp',
-    summary: 'Summary of Activity 1'
+    title: 'Beautiful view',
+    imageUrl: '/traveler-photos/pic-1.webp'
   },
   {
-    id: 'activity-2',
-    title: 'Activity 2',
-    description: 'Description of Activity 2',
-    imageUrl: '/traveler-photos/pic-2.webp',
-    summary: 'Summary of Activity 2'
+    title: 'Having fun',
+    imageUrl: '/traveler-photos/pic-2.webp'
   },
   {
-    id: 'activity-3',
-    title: 'Activity 3',
-    description: 'Description of Activity 3',
-    imageUrl: '/traveler-photos/pic-3.webp',
-    summary: 'Summary of Activity 3'
+    title: 'With goofy and friends',
+    imageUrl: '/traveler-photos/pic-3.webp'
   },
   {
-    id: 'activity-1',
-    title: 'Activity 1',
-    description: 'Description of Activity 1',
-    imageUrl: '/traveler-photos/pic-1.webp',
-    summary: 'Summary of Activity 1'
+    title: 'Beautiful view',
+    imageUrl: '/traveler-photos/pic-1.webp'
   },
   {
-    id: 'activity-2',
-    title: 'Activity 2',
-    description: 'Description of Activity 2',
-    imageUrl: '/traveler-photos/pic-2.webp',
-    summary: 'Summary of Activity 2'
+    title: 'Having fun',
+    imageUrl: '/traveler-photos/pic-2.webp'
   },
   {
-    id: 'activity-3',
-    title: 'Activity 3',
-    description: 'Description of Activity 3',
-    imageUrl: '/traveler-photos/pic-3.webp',
-    summary: 'Summary of Activity 3'
+    title: 'With goofy and friends',
+    imageUrl: '/traveler-photos/pic-3.webp'
   },
   {
-    id: 'activity-2',
-    title: 'Activity 2',
-    description: 'Description of Activity 2',
-    imageUrl: '/traveler-photos/pic-2.webp',
-    summary: 'Summary of Activity 2'
+    title: 'Having fun',
+    imageUrl: '/traveler-photos/pic-2.webp'
   },
   {
-    id: 'activity-3',
-    title: 'Activity 3',
-    description: 'Description of Activity 3',
-    imageUrl: '/traveler-photos/pic-3.webp',
-    summary: 'Summary of Activity 3'
+    title: 'With goofy and friends',
+    imageUrl: '/traveler-photos/pic-3.webp'
   }
 ];
